d8bdd807ed8b193110000c1f45b6f1b7945a50f0
haiku <401952452@qq.com>
2019-08-17 09:16:37 +0800

    1068 delete v

--- 1068 Find More Coins.cpp
+++ 1068 Find More Coins.cpp
@@ -16,12 +16,11 @@
 #include<stack>
 using namespace std;
 bool dp[105][10005];
-bool v[105][10005];
 int a[10005];
 vector<int> ans;
-int findfirst(bool arr[], int size) {
-	for (int i = size; i >= 0 ; --i) {
-		if (arr[i]) return i;
+int findfirst(int m, int size) {
+	for (int i = size; i >= 1 ; --i) {
+		if (a[i]<=m && dp[m-a[i]][i-1]) return i;
 	}
 	return 0;
 }
@@ -34,23 +33,18 @@ int main()
 	for (int i = 0; i <= n; ++i) dp[0][i] = true;
 	for (int i = 1; i <= m; ++i) {
 		for (int j = 1; j <= n; ++j) {
-			if (a[j] > i) {
-				dp[i][j] = dp[i][j - 1];
-			}
-			else {
-				dp[i][j] = dp[i - a[j]][j - 1] || dp[i][j - 1];
-				if (dp[i - a[j]][j - 1]) v[i][j] = true;
-			}
+			if (a[j] > i) dp[i][j] = dp[i][j - 1];
+			else dp[i][j] = dp[i - a[j]][j - 1] || dp[i][j - 1];
 		}
 	}
 	while (m) {
-		int i = findfirst(v[m], n);
-		n = i - 1;
+		int i = findfirst(m, n);
 		if (!i) {
 			printf("No Solution\n");
 			break;
 		}
 		ans.push_back(a[i]);
+        n = i - 1;
 		m -= a[i];
 	}
 	for (int i = 0; i < ans.size(); ++i) {
